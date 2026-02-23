268b92f7e2c3a623770f2de998e83e10bb7385cd
Makarrco <kotmakar111@gmail.com>
2026-02-23 21:12:07 +0200

    Rendering

--- MaleFashionApp/MaleFashionApp/Controllers/AboutController.cs
+++ MaleFashionApp/MaleFashionApp/Controllers/AboutController.cs
@@ -1,10 +1,22 @@
-﻿using MaleFashionApp.Entities;
+﻿using System.Diagnostics;
+using MaleFashionApp.DB;
+using MaleFashionApp.Entities;
+using MaleFashionApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MaleFashionApp.Controllers;
 
 public class AboutController : Controller
 {
+    private ClothingShopDbContext _clothingDbContext;
+    
+    private FormModel _formModel;
+    public AboutController(ClothingShopDbContext clothingDbContext)
+    {
+        _clothingDbContext = clothingDbContext;
+        _formModel = new FormModel(clothingDbContext);
+    }
+    
     [HttpGet]
     public IActionResult Index()
     {
@@ -16,18 +28,21 @@ public class AboutController : Controller
         return View();
     }
 
-    // [HttpPost]
-    // public IActionResult SaveClientMessage(string Title, string Email, string Message)
-    // {
-    //     return View();
-    // }
+    
     [HttpPost]
     public IActionResult SaveClientMessage(Form clientMessage)
     {
         if (ModelState.IsValid)
         {
-            // saving in DB
-            return View("Thanks", clientMessage);
+            if (_formModel.SaveForm(clientMessage))
+            {
+                return View("Thanks",  clientMessage);
+            }
+            else
+            {
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+                
+            }
         } else {
             return View("Contactus");
         }

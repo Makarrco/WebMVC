8de4e9f7994763efaa975c9f2eebdf9ff6e899eb
Makarrco <kotmakar111@gmail.com>
2026-02-23 21:13:14 +0200

    ComponentsRenderingEnd

--- MaleFashionApp/MaleFashionApp/ViewComponents/ShoppingTwoViewComponent.cs
+++ MaleFashionApp/MaleFashionApp/ViewComponents/ShoppingTwoViewComponent.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MaleFashionApp.DB;
+using MaleFashionApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MaleFashionApp.ViewComponents;
 
 public class ShoppingTwoViewComponent : ViewComponent
 {
+    private readonly ClothingShopDbContext _clothingDbContext;
+    private OptionModel _optionModel;
+
+    public ShoppingTwoViewComponent(ClothingShopDbContext clothingDbContext)
+    {
+        _clothingDbContext = clothingDbContext;
+        _optionModel = new OptionModel(clothingDbContext);
+    }
+
     public IViewComponentResult Invoke()
     {
-        return View("ShoppingTwo");
+        ShoppingTwoViewModel viewModel = new ShoppingTwoViewModel();
+
+        viewModel.Shopping = _optionModel.GetShopping();
+        viewModel.ContactUs = _optionModel.GetContactUs();
+        viewModel.PaymentMethods = _optionModel.GetPaymentMethods();
+        viewModel.Delivery = _optionModel.GetDelivery();
+        viewModel.ReturnExchanges = _optionModel.GetReturnExchanges();
+
+        return View("ShoppingTwo", viewModel);
     }
 }
--- MaleFashionApp/MaleFashionApp/ViewComponents/ShoppingViewComponent.cs
+++ MaleFashionApp/MaleFashionApp/ViewComponents/ShoppingViewComponent.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MaleFashionApp.DB;
+using MaleFashionApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MaleFashionApp.ViewComponents;
 
 public class ShoppingViewComponent : ViewComponent
 {
+    private readonly ClothingShopDbContext _clothingDbContext;
+    private OptionModel _optionModel;
+
+    public ShoppingViewComponent(ClothingShopDbContext clothingDbContext)
+    {
+        _clothingDbContext = clothingDbContext;
+        _optionModel = new OptionModel(clothingDbContext);
+    }
     public IViewComponentResult Invoke()
     {
-        return View("Shopping");
+        ShoppingViewModel shoppingViewModel = new ShoppingViewModel();
+
+        shoppingViewModel.Shopping = _optionModel.GetShopping();
+        shoppingViewModel.ClothStore = _optionModel.GetClothStore();
+        shoppingViewModel.TrendShoes = _optionModel.GetShoes();
+        shoppingViewModel.Accessories = _optionModel.GetAccessories();
+        shoppingViewModel.Sales = _optionModel.GetSales();
+        return View("Shopping",  shoppingViewModel);
     }
 }

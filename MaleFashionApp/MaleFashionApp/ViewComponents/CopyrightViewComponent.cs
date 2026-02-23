e306283f287fe23a110d5a398405c3df43e17fef
Makarrco <kotmakar111@gmail.com>
2026-02-23 21:13:56 +0200

    BlogPageRenders

--- MaleFashionApp/MaleFashionApp/ViewComponents/CopyrightViewComponent.cs
+++ MaleFashionApp/MaleFashionApp/ViewComponents/CopyrightViewComponent.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace MaleFashionApp.ViewComponents;
+﻿using MaleFashionApp.DB;
+using MaleFashionApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 public class CopyrightViewComponent : ViewComponent
 {
+    private readonly ClothingShopDbContext _clothingDbContext;
+    private OptionModel _optionModel;
+
+    public CopyrightViewComponent(ClothingShopDbContext clothingDbContext)
+    {
+        _clothingDbContext = clothingDbContext;
+        _optionModel = new OptionModel(clothingDbContext);
+    }
+
     public IViewComponentResult Invoke()
     {
-        return View("Copyright");
+        var option = _optionModel.GetCopyright();
+        return View("Copyright", option);
     }
 }
